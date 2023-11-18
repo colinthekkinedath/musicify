6f2adb830ebfe323b072803afe8986c65fde3d4e
colinthekkinedath <colinthekkinedath@gmail.com>
2023-11-18 14:46:24 -0800

    added rec card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import Hero from './components/Hero'
-
+import Card from './components/Card'
 export default function Home() {
   return (
     <main className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
         <Hero />
+        <Card image={"https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228"} title ={"title"} artist={"artist"} album={"album"} genre={"genre"} />
     </main>
 
   )
